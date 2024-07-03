309208a583ab906e0304f265390bb994563256f7
yacine <yacinomar999@gmail.com>
2024-07-03 21:22:53 +0100

    add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Hero from "@/components/main/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>thomething</div>
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero/>
+      </div>
+    </main>
   );
 }
