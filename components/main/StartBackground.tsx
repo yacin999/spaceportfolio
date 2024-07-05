366702bf64bcf652302a3b1edd4bc17b3aad0c62
yacine <yacinomar999@gmail.com>
2024-07-05 11:44:48 +0100

    create navbar

--- components/main/StartBackground.tsx
+++ components/main/StartBackground.tsx
@@ -4,7 +4,7 @@ import React, {useState, useRef, Suspense} from 'react'
 import { Canvas, useFrame } from "@react-three/fiber"
 import { Points, PointMaterial, Preload } from "@react-three/drei"
 //@ts-ignore
-import * as random from "math/random/dist/math-random.esm"
+import * as random from "maath/random/dist/maath-random.esm";
 
 type Props = {}
 
