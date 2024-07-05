2712736307d3715d58b7d00e46671670c40fe0e2
yacine <yacinomar999@gmail.com>
2024-07-05 22:00:03 +0100

    crreate skill text

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,4 +1,6 @@
+import { Backend_skill, Frontend_skill, Full_stack, Other_skill } from '@/constants'
 import React from 'react'
+import SkillDataProvider from '../sub/SkillDataProvider'
 
 type Props = {}
 
@@ -10,7 +12,65 @@ const Skills = (props: Props) => {
         style={{transform : "scale(0.9)"}}
     >
         <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
-            
+            {Frontend_skill.map((image, index)=> (
+                <SkillDataProvider
+                    key={index}
+                    src={image.Image}
+                    width={image.width}
+                    height={image.height}
+                    index={index}
+                />
+            ))}
+        </div>
+
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+            {Backend_skill.map((image, index)=> (
+                <SkillDataProvider
+                    key={index}
+                    src={image.Image}
+                    width={image.width}
+                    height={image.height}
+                    index={index}
+                />
+            ))}
+        </div>
+
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+            {Full_stack.map((image, index)=> (
+                <SkillDataProvider
+                    key={index}
+                    src={image.Image}
+                    width={image.width}
+                    height={image.height}
+                    index={index}
+                />
+            ))}
+        </div>
+
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+            {Other_skill.map((image, index)=> (
+                <SkillDataProvider
+                    key={index}
+                    src={image.Image}
+                    width={image.width}
+                    height={image.height}
+                    index={index}
+                />
+            ))}
+        </div>
+
+        <div className='w-full h-full absolute'>
+            <div className='w-full h-full z-[-10] opacity-30 absolute flex itemce justify-center bg-cover'>
+                <video
+                    className='w-full h-auto '
+                    preload='false'
+                    playsInline
+                    loop
+                    muted
+                    autoPlay
+                    src='/cards-video.webm'
+                />
+            </div>
         </div>
     </section>
   )
