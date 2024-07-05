63318c0795696f3ee0e31b0c9e4682b852caaf22
yacine <yacinomar999@gmail.com>
2024-07-05 21:39:58 +0100

    create skill data provider

---
+++ components/main/Skills.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+const Skills = (props: Props) => {
+  return (
+    <section
+        id='skills'
+        className='flex flex-col items-center justify-center gap-3 h-full relative overflew-hidden py-20'
+        style={{transform : "scale(0.9)"}}
+    >
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Skills
