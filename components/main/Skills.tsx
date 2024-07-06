61866dba49c53210946f1d2462b2cffd10616cfd
yacine <yacinomar999@gmail.com>
2024-07-06 10:33:19 +0100

    create Encryption

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,6 +1,7 @@
-import { Backend_skill, Frontend_skill, Full_stack, Other_skill } from '@/constants'
+import { Backend_skill, Frontend_skill, Full_stack, Other_skill, Skill_data } from '@/constants'
 import React from 'react'
 import SkillDataProvider from '../sub/SkillDataProvider'
+import SkillText from '../sub/SkillText'
 
 type Props = {}
 
@@ -11,6 +12,20 @@ const Skills = (props: Props) => {
         className='flex flex-col items-center justify-center gap-3 h-full relative overflew-hidden py-20'
         style={{transform : "scale(0.9)"}}
     >
+        <SkillText/>
+
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+            {Skill_data.map((image, index)=> (
+                <SkillDataProvider
+                    key={index}
+                    src={image.Image}
+                    width={image.width}
+                    height={image.height}
+                    index={index}
+                />
+            ))}
+        </div>
+
         <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
             {Frontend_skill.map((image, index)=> (
                 <SkillDataProvider
@@ -62,7 +77,7 @@ const Skills = (props: Props) => {
         <div className='w-full h-full absolute'>
             <div className='w-full h-full z-[-10] opacity-30 absolute flex itemce justify-center bg-cover'>
                 <video
-                    className='w-full h-auto '
+                    className='w-full h-auto'
                     preload='false'
                     playsInline
                     loop
