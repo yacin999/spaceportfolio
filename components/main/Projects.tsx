c5c569866de20ca76bdadc7a3a000593707c7882
yacine <yacinomar999@gmail.com>
2024-07-06 12:11:52 +0100

    projects completed

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import ProjectCard from '../sub/ProjectCard'
 
 type Props = {}
 
 const Projects = (props: Props) => {
   return (
-    <div>Projects</div>
+    <div className='flex flex-col items-center justify-center py-20'>
+        <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>My Projects</h1>
+
+        <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
+            <ProjectCard
+                src='NextWebsite.png'
+                title='Modern next js Portfolio'
+                description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla veritatis illum illo ut consequatur, libero, consequuntur provident tempora doloremque, iure odit quas eius! In autem perferendis eum! Esse, voluptatibus sapiente?'
+            />
+
+            <ProjectCard
+                src='CardImage.png'
+                title='Interactive Website Cards'
+                description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla veritatis illum illo ut consequatur, libero, consequuntur provident tempora doloremque, iure odit quas eius! In autem perferendis eum! Esse, voluptatibus sapiente?'
+            />
+
+            <ProjectCard
+                src='SpaceWebsite.png'
+                title='Space Themed Website'
+                description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla veritatis illum illo ut consequatur, libero, consequuntur provident tempora doloremque, iure odit quas eius! In autem perferendis eum! Esse, voluptatibus sapiente?'
+            />
+            
+        </div>
+        
+    </div>
   )
 }
 
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,10 +1,31 @@
+"use client"
+
+import Image from 'next/image'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    src : string, 
+    title : string,
+    description : string
+}
 
-const ProjectCard = (props: Props) => {
-  return (
-    <div>ProjectCard</div>
+const ProjectCard = ({src, title, description}: Props) => {
+
+  
+    return (
+    <div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]'>
+        <Image
+            src={src}
+            alt={title}
+            width={1000}
+            height={1000}
+            className='w-full object-contain'
+        />
+        <div className='relative p-4'>
+            <h1 className='text-2xl font-semibold text-white'>{title}</h1>
+            <p className='mt-2 text-gray-300'>{description}</p>
+        </div>
+    </div>
   )
 }
 
