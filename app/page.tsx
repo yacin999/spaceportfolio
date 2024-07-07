004f8f425f0a7b99072f3c38ecb4439da2cf6d3f
yacine <yacinomar999@gmail.com>
2024-07-07 09:43:09 +0100

    add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
+        <Hero/>
         <Skills/>
         <Encryption/>
         <Projects/>
